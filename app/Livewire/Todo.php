59048edb8f6232aa36192ad78bd5c36184272507
slawek-maslowski <sla.maslowski@gmail.com>
2024-01-30 17:01:27 +0100

    finish

--- app/Livewire/Todo.php
+++ app/Livewire/Todo.php
@@ -2,20 +2,32 @@
 
 namespace App\Livewire;
 
+use App\Models\User;
+use Auth;
 use Illuminate\Contracts\View\View;
 use Livewire\Attributes\Validate;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class Todo extends Component
 {
+    use WithPagination;
+
     #[Validate('required|min:5')]
-    public string $todo = '';
+    public string $todo = ''; // zmienna z formularza dodawania
 
     public bool $success = false;
 
+    public int $edit; // id edytowanej pozycji
+
+    #[Validate('required|min:5')]
+    public string $edited; // edytowana tresc
+
     public function render(): View
     {
-        return view('livewire.todo');
+        $todos = auth()->user()->todos()->latest()->paginate(3);
+
+        return view('livewire.todo', compact('todos'));
     }
 
     public function save(): void
@@ -28,5 +40,36 @@ class Todo extends Component
 
         $this->success = true;
         $this->reset('todo');
+    }
+
+    public function editId($edit): void
+    {
+        $this->edit = $edit;
+        $this->edited = auth()->user()->todos()->find($edit)->todo;
+    }
+
+    public function cancel()
+    {
+        $this->reset('edit');
+    }
+
+    public function delete($edit)
+    {
+        $todo = auth()->user()->todos()->find($edit);
+        $todo->delete();
+    }
+
+    public function mark($edit)
+    {
+        $todo = auth()->user()->todos()->find($edit);
+        return ($todo->completed) ? $todo->update(['completed' => false]) : $todo->update(['completed' => true]);
+    }
+
+    public function update($edit)
+    {
+        $this->validateOnly('edited');
+        $todo = auth()->user()->todos()->find($edit);
+        $todo->update(['todo' => $this->edited]);
+        $this->reset('edit');
     }
 }
--- resources/views/livewire/todo.blade.php
+++ resources/views/livewire/todo.blade.php
@@ -4,44 +4,59 @@
             <span class="block mb-4 text-green-500">Notatka dodana.</span>
         @endif
     </div>
-    <form class="flex" method="POST" wire:submit.prevent="save">
+    <div class="text-center pb-2">Dodaj coś do listy</div>
+    <form class="flex border-solid border-gray-100 border-b-2 pb-4" method="POST" wire:submit.prevent="save">
         <x-text-input wire:model="todo" id="todo" class="w-full mr-2"/>
         @error('todo')
         <span class="mt-2 text-sm text-red-600">{{ $message }}</span>
         @enderror
         <x-primary-button>
-            Add
+            Dodaj
         </x-primary-button>
     </form>
     <br>
 
-
-    {{-- First todo --}}
-    <div class="flex mt-5 py-4 justify-center justify-between">
-        <div>
-            <input id="green-checkbox" type="checkbox"
-                   class="w-4 h-4 text-green-600 bg-gray-100 border-gray-300 rounded focus:ring-green-500 dark:focus:ring-green-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-
+    @foreach($todos as $todo)
+        <div class="flex mt-5 py-4 justify-center justify-between">
+            <div>
+                <input id="green-checkbox" wire:click="mark({{ $todo->id }})" @if($todo->completed) checked
+                       @endif type="checkbox"
+                       class="w-4 h-4 text-green-600 bg-gray-100 border-gray-300 rounded focus:ring-green-500 dark:focus:ring-green-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+            </div>
+            <div>
+                @if($edit==$todo->id)
+                    <x-text-input wire:model="edited" id="todo" class="w-full mr-2"/>
+                    @error('edited')
+                    <span class="mt-2 text-sm text-red-600">{{ $message }}</span>
+                    @enderror
+                @else
+                    {{ $todo->todo }}
+                @endif
+            </div>
+            <div>
+                @if($edit==$todo->id)
+                    <x-secondary-button wire:click="update({{ $todo->id }})">
+                        Zmień
+                    </x-secondary-button>
+                    <x-danger-button wire:click=" cancel">
+                        Anuluj
+                    </x-danger-button>
+                @else
+                    <x-secondary-button wire:click="editId({{ $todo->id }})">
+                        Edytuj
+                    </x-secondary-button>
+                    <x-danger-button wire:click="delete({{ $todo->id }})">
+                        Usuń
+                    </x-danger-button>
+                @endif
+            </div>
         </div>
-        <div>
-            todo
-        </div>
-
-        <div>
-            <x-secondary-button>
-                Edit
-            </x-secondary-button>
-            <x-danger-button>
-                Delete
-            </x-danger-button>
-        </div>
-
-    </div>
+    @endforeach
 
 
     <div class="mt-5">
         <div>
-
+            {{ $todos->links() }}
         </div>
     </div>
 </div>
